ff4eb30c992cf02f28f04052ad855421c57470c6
okoking <144639739+okoking@users.noreply.github.com>
2024-04-02 16:02:27 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,8 @@
 #include "Scene/Title/Title.h"
 
 // Win32アプリケーションは WinMain関数 から始まる
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-
 	//ウィンドウの名前を変える
 	SetMainWindowText("釣りキング");
 
