2817605a61ddd2f5318d41c59cb7f6b8e182118a
100-oC <mitotonngoria@outlook.jp>
2024-04-02 14:28:59 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 //ウィンドウを表示するプログラム（ひな形）
 
 #include "DxLib.h"	//DXライブラリのインクルード
-<<<<<<< HEAD
 #include "FrameRate/FrameRate.h"	//フレームレート
 #include "Input/Input.h"	//入力機能
-=======
 #include "Scene/Scene.h"	
->>>>>>> 50cfde0d2cb620eb81f871ee6c4ed0e62cfb00bf
 
 // define
 #define	SCREEN_SIZE_X	640	// X方向の画面サイズを指定
@@ -61,7 +58,51 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			//ここからゲームの本体を書くことになる
 			//-----------------------------------------
 
+			//火山
+			//シーン遷移
+			switch (g_CurrentSceneID)
+			{
+				//タイトルシーン初期化
+			case SCENE_ID_INIT_TITLE:
 
+				break;
+				//タイトルシーン通常処理
+			case SCENE_ID_LOOP_TITLE:
+
+				break;
+				//タイトルシーン終了処理
+			case SCENE_ID_FIN_TITLE:
+
+				break;
+
+
+				//プレイシーン初期化
+			case SCENE_ID_INIT_PLAY:
+
+				break;
+				//プレイシーン通常処理
+			case SCENE_ID_LOOP_PLAY:
+
+				break;
+				//プレイシーン終了処理
+			case SCENE_ID_FIN_PLAY:
+
+				break;
+
+
+				//リザルトシーン初期化処理
+			case SCENE_ID_INIT_RESULT:
+
+				break;
+				//リザルトシーン通常処理
+			case SCENE_ID_LOOP_RESULT:
+
+				break;
+				//リザルトシーン終了処理
+			case SCENE_ID_FIN_RESULT:
+
+				break;
+			}
 
 
 			//-----------------------------------------
@@ -76,69 +117,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			//フリップ関数
 			ScreenFlip();
 		}
-<<<<<<< HEAD
-=======
-
-		//画面に表示されたものを初期化
-		ClearDrawScreen();
-
-		//-----------------------------------------
-		//ここからゲームの本体を書くことになる
-		//-----------------------------------------
-
-		//火山
-		//シーン遷移
-		switch (g_CurrentSceneID)
-		{
-			//タイトルシーン初期化
-			case SCENE_ID_INIT_TITLE:
-
-			break;
-			//タイトルシーン通常処理
-			case SCENE_ID_LOOP_TITLE:
-
-			break;
-			//タイトルシーン終了処理
-			case SCENE_ID_FIN_TITLE:
-
-			break;
-
-
-			//プレイシーン初期化
-			case SCENE_ID_INIT_PLAY:
-
-			break;
-			//プレイシーン通常処理
-			case SCENE_ID_LOOP_PLAY:
-
-			break;
-			//プレイシーン終了処理
-			case SCENE_ID_FIN_PLAY:
-
-			break;
-
-
-			//リザルトシーン初期化処理
-			case SCENE_ID_INIT_RESULT:
-
-			break;
-			//リザルトシーン通常処理
-			case SCENE_ID_LOOP_RESULT:
-
-			break;
-			//リザルトシーン終了処理
-			case SCENE_ID_FIN_RESULT:
-
-			break;
-		}
-
-
-		//-----------------------------------------
-		//ループの終わりに
-		//フリップ関数
-		ScreenFlip();
-
->>>>>>> 50cfde0d2cb620eb81f871ee6c4ed0e62cfb00bf
 	}
 
 	//-----------------------------------------
